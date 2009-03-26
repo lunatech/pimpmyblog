ffe1a60c51d47faad2761d9d09290cf5b151a363
Raj (lunatech) Shekhar <spamme@rajshekhar.net>
2009-03-25 22:07:44 -0700

    experimented with xpath on this branch.  However, did not see any difference  in the speed in using xpath

--- pimpmyblog.php
+++ pimpmyblog.php
@@ -10,17 +10,29 @@ function fetch_url($url)
   return $res;
 }
 
+
+function fetch_last_post_xpath($blog_xml)
+{
+  $h_xml = simplexml_load_string($blog_xml);
+  $t = $h_xml->xpath('/rss/channel/item[1]/title');
+  $result['title'] = (string)$t[0];
+  //var_dump($result);
+  return $result;
+}
+
 function fetch_last_post($blog_xml)
 {
   $h_xml = simplexml_load_string($blog_xml);
   $result['title'] = (string)$h_xml->channel->item[0]->title;
+  $result['title'] = (string)$h_xml->xpath('/rss/channel/item');
   $result['link'] = (string)$h_xml->channel->item[0]->link;
   $result['pubDate'] = (string)$h_xml->channel->item[0]->pubDate;
+  $result = $h_xml->xpath('/rss/channel/item[1]/title');
   return $result;
 }
 
-$blog_rss_url = "http://rajshekhar.net/blog/feeds/index.rss2";
+$blog_rss_url = "http://joomladev.rajshekhar.net/index.php?format=feed&type=rss";
 $blog_xml = fetch_url($blog_rss_url);
-$last_blog = fetch_last_post($blog_xml);
-print_r($last_blog);
+$last_blog_xpath = fetch_last_post_xpath($blog_xml);
+//$last_blog = fetch_last_post($blog_xml);
 ?>
